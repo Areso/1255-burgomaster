ea0885151009ebb12be4c10d59f5772ee7418369
Areso <returnstrike@yandex.ru>
2019-04-21 19:01:06 +0500

    done translating string resources

--- en-US.js
+++ en-US.js
@@ -17,4 +17,32 @@ localeStrings.push("Sorry! There is no save to load from!");
 localeStrings.push("Save game? It will rewrite any save you already have!");//15
 localeStrings.push("Please insert save");
 localeStrings.push("very long string");
+localeStrings.push("Do you want to Hire a guardsman? It will cost you 20 gold and then 20 gold per each season!");
+localeStrings.push("You hired a guardsman!");
+localeStrings.push("Not enough gold to do that!");//20
+localeStrings.push("Treasury is required to hiring a guardsman!");
+localeStrings.push("Would you like to dismiss a guardsman?");
+localeStrings.push("You dismissed a guardsman!");
+localeStrings.push("You don't have any guardsmen!");
+localeStrings.push("treachery");//25
+localeStrings.push("betrayal");
+localeStrings.push("corruption");
+localeStrings.push("bribery");
+localeStrings.push("graft and theft");
+localeStrings.push("embezzlement");//30
+localeStrings.push("placeholder");
+localeStrings.push("placeholder");
+localeStrings.push("placeholder");
+localeStrings.push("you executed a citizen for %arg1");
+localeStrings.push("treasury funded on %arg1 with confiscated property of the executed citizen");//35
+localeStrings.push("too few citizens");
+localeStrings.push("treasury guard(s) dismissed because you are cannot pay to them!");
+localeStrings.push("fire in the city!");
+localeStrings.push("Call firemen? It will cost you %arg1 gold!");
+localeStrings.push("firemen put end to the fire!");//40
+localeStrings.push("it will burn to ashes!");
+localeStrings.push("shit happens: thieves stole %arg1 gold from your treasury");
+localeStrings.push("You won in a lottery %arg1 gold prize");
+
+
 
--- ru-RU.js
+++ ru-RU.js
@@ -17,3 +17,29 @@ localeStrings.push("Извините, но сохраненных игр нет!
 localeStrings.push("Сохранить игру? Это перезапишет текущее сохранение, если у вас такое есть");//15
 localeStrings.push("Пожалуйста, вставьте сохранение");
 localeStrings.push("очень длинная строка");
+localeStrings.push("Вы хотите нанять охранника? Это будет стоить 20 золотых сразу и 20 золотых за каждый сезон");
+localeStrings.push("Вы наняли охранника");
+localeStrings.push("Недостаточно золота!");//20
+localeStrings.push("Для найма охранника необходима Сокровищница!");
+localeStrings.push("Уволить охранника?");
+localeStrings.push("Вы уволили охранника");
+localeStrings.push("У вас нет охранников");
+localeStrings.push("предательство");//25
+localeStrings.push("шпионаж");
+localeStrings.push("коррупцию");
+localeStrings.push("подкуп");
+localeStrings.push("казнокрадство");
+localeStrings.push("кражи");//30
+localeStrings.push("placeholder");
+localeStrings.push("placeholder");
+localeStrings.push("placeholder");
+localeStrings.push("вы казнили горожанина за %arg1");//34
+localeStrings.push("казна пополниналась на %arg1 золотых после конфискации имущества");//35
+localeStrings.push("слишком мало жителей");
+localeStrings.push("охранники уволились, потому что вам нечем платить им!");
+localeStrings.push("пожар в городе!");
+localeStrings.push("Вызвать пожарных? Это будет стоить %arg1 золота");
+localeStrings.push("пожарные погасили огонь");//40
+localeStrings.push("будет гореть дотла!");
+localeStrings.push("фигня случается: воры украли %arg1 золота из вашей Сокровищницы");
+localeStrings.push("вы выиграли в лоттерею %arg1 золота");
