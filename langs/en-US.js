119a27b37dba2ede57bc1194f3aaf03bb6f6642b
Areso <returnstrike@yandex.ru>
2019-11-10 15:10:38 +0300

    returned hero don't overflow threasury now fix #378

--- langs/en-US.js
+++ langs/en-US.js
@@ -88,7 +88,7 @@ localeStrings.push("Your UID");//85
 localeStrings.push("Your nickname");
 localeStrings.push("Update");
 localeStrings.push(["Your nickname is registered","Your nickname is updated","Sorry, but this nicknames is taken"]);
-localeStrings.push("placeholder");
+localeStrings.push("%arg1 gold has been added to your treasury");
 localeStrings.push("placeholder");//90
 localeStrings.push("Garrison");
 localeStrings.push("Fire");
