454838888fcbe12aa8d80834bfc82e674cb9c273
Anton Gladyshev <returnstrike@yandex.ru>
2021-10-25 01:49:44 +0300

    infoblock for hiring is done

--- dom.js
+++ dom.js
@@ -3,14 +3,56 @@ function selectObj (myobject) {
 	for (mi=0;mi<matches.length;mi++){
 		matches[mi].style.background="black";
 	}
+	infoblock = document.getElementById("grid-info"); 
 	if (myobject.id==="imgHiringScreenSergeantToHire") {
 		document.getElementById("imgHiringScreenSergeantToHire").style.background="yellow";
+		infoblock.innerHTML  = localeStrings[308].replace("%arg1", config.sergeantHiring).replace("%arg2", config.sergeantUpkeep);
+		infoblock.innerHTML += '<div class="unit-info">\
+								<p>Atk.: <span id="sergeantsAttack">5</span></p>\
+								<p>Def.: <span id="sergeantsDefence">8</span></p>\
+								<p>Dmg: 3-4</p>\
+								<p>HP: <span id="sergeantsHealth">15</span></p></div>';
+		infoblock.innerHTML += '<input type="range" id="hireNumberRange" value="1" min="1" max="100"\
+								oninput="hireNumberValue.value = this.value">';
+		infoblock.innerHTML += '<input type="text"  id="hireNumberValue" value="1"><br>';
+		infoblock.innerHTML += '<button onclick="game.hireUnits()">Hire</button>';
 	}
 	if (myobject.id==="imgHiringScreenTurkopolToHire") {
 		document.getElementById("imgHiringScreenTurkopolToHire").style.background="yellow";
+		infoblock.innerHTML  = localeStrings[309].replace("%arg1", config.sergeantHiring).replace("%arg2", config.sergeantUpkeep);
+		infoblock.innerHTML += '<div class="unit-info">\
+								<p>Atk.: <span id="turkopolsAttack">5</span></p>\
+								<p>Def.: <span id="turkopolsDefence">5</span></p>\
+								<p>Dmg: 2-3</p>\
+								<p>HP: <span id="turkopolsHealth">12</span></p></div>';
+		if (game.buildLevelArchery>0){
+			infoblock.innerHTML += '<input type="range" id="hireNumberRange" value="1" min="1" max="100"\
+									oninput="hireNumberValue.value = this.value">';
+			infoblock.innerHTML += '<input type="text"  id="hireNumberValue" value="1"><br>';
+			infoblock.innerHTML += '<button onclick="game.hireUnits()">Hire</button>';
+		} else {
+			msg = "<b>%arg1</b>";
+			infoblock.innerHTML += msg.replace("%arg1", localeStrings[317]);
+		}
 	}
 	if (myobject.id==="imgHiringScreenKnightToHire") {
 		document.getElementById("imgHiringScreenKnightToHire").style.background="yellow";
+		infoblock.innerHTML  = localeStrings[310].replace("%arg1", config.sergeantHiring).replace("%arg2", config.sergeantUpkeep);
+		infoblock.innerHTML += '<div class="unit-info">\
+								<p>Atk.: <span id="knightsAttack">10</span></p>\
+								<p>Def.: <span id="knightsDefence">9</span></p>\
+								<p>Dmg: 5-10</p>\
+								<p>HP: <span id="knightsHealth">20</span></p></div>';
+		if (game.buildLevelStables>1){
+			infoblock.innerHTML += '<input type="range" id="hireNumberRange" value="1" min="1" max="100"\
+									oninput="hireNumberValue.value = this.value">';
+			infoblock.innerHTML += '<input type="text"  id="hireNumberValue" value="1"><br>';
+			infoblock.innerHTML += '<button onclick="game.hireUnits()">Hire</button>';
+		} else {
+			msg = "<b>%arg1</b>";
+			msg = msg.replace("%arg1",localeStrings[318].replace("%arg1", 2));
+			infoblock.innerHTML += msg;
+		}
 	}
 }
 function hoverOutObj(myobject) {
