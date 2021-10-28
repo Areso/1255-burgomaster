adb1838e0b9220dfa5c3a03309d4dfd92adbce47
Anton Gladyshev <returnstrike@yandex.ru>
2021-10-28 13:51:33 +0300

    moved some code

--- dom.js
+++ dom.js
@@ -18,3 +18,31 @@ function setLogin(){
 	document.getElementById("emailLine").style.display = "none";
 	document.getElementById("btnRegLogin").innerText   = "Login";
 };
+function openChat() {
+	log_dom   = document.getElementById("console_n_chat");
+	chat_dom  = document.getElementById("chat");
+	nick_dom  = document.getElementById("inp_nickname");
+	msg_dom   = document.getElementById("msg_out");
+	btn_send  = document.getElementById("btnSend");
+	log_dom.style.visibility  = "hidden";
+	chat_dom.style.visibility = "visible";
+	nick_dom.style.visibility = "visible";
+	msg_dom.style.visibility  = "visible";
+	btn_send.style.visibility = "visible";
+}
+function openLog() {
+	log_dom   = document.getElementById("console_n_chat");
+	chat_dom  = document.getElementById("chat");
+	nick_dom  = document.getElementById("inp_nickname");
+	msg_dom   = document.getElementById("msg_out");
+	btn_send  = document.getElementById("btnSend");
+	log_dom.style.visibility  = "visible";
+	chat_dom.style.visibility = "hidden";
+	nick_dom.style.visibility = "hidden";
+	msg_dom.style.visibility  = "hidden";
+	btn_send.style.visibility = "hidden";
+}
+function checkFirebrigade() {
+	document.getElementById("lblFBUpkeepPriceValue").innerHTML = game.fireGuardUpkeep();
+	openTab(null, 'tabFirebrigade');
+}
