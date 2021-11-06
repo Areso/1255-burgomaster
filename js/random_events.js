6ab7464b361d80948a321b05274159827d448bf5
Anton Gladyshev <returnstrike@yandex.ru>
2021-11-06 17:25:25 +0300

    random events now live in separate place

---
+++ js/random_events.js
@@ -0,0 +1,45 @@
+setInterval(rndEvents, 100000);
+function rndEvents() {
+	var rnd = Math.floor((Math.random() * 12) + 1);
+	//rnd = 2;//debug
+	switch (rnd) {
+		case 1:
+			game.theftFromTreasury();
+			break;
+		case 2:
+			game.startFire();
+			break;
+		case 3:
+			game.startPlague();
+			break;
+		case 4:
+			game.chestCityAppear();
+			break;
+		case 5:
+			//
+			break;
+		case 6:
+			//
+			break;
+		case 7:
+			//
+			break;
+		case 8:
+			//
+			break;
+		case 9:
+			//
+			break;
+		case 10:
+				//
+			break;
+		case 11:
+			//
+			break;
+		case 12:
+			game.winLottery();
+			break;
+		default:
+			alert("error on switch/case function");
+		}
+	}
