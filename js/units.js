8d142b26710fb09869437729135ab88011dee358
Anton Gladyshev <returnstrike@yandex.ru>
2021-11-06 18:02:23 +0300

    units now live in separate file

---
+++ js/units.js
@@ -0,0 +1,80 @@
+	/*-- New: Units data --*/
+	var sergeantsData = {
+	  name: 'sergeants',
+	  minDmg: 3,
+	  maxDmg: 4,
+	  attack: 5,
+	  defence: 8,
+	  health: 15,
+	  count: 1
+	};
+
+	var turkopolsData = {
+	  name: 'turkopols',
+	  minDmg: 2,
+	  maxDmg: 3,
+	  attack: 5,
+	  defence: 5,
+	  health: 12,
+	  count: 1
+	};
+
+	var knightsData = {
+	  name: 'knights',
+	  minDmg: 5,
+	  maxDmg: 10,
+	  attack: 10,
+	  defence: 9,
+	  health: 20,
+	  count: 1
+	};
+
+	var banditWarrior = {
+	  name: 'bandit',
+	  minDmg: 1,
+	  maxDmg: 3,
+	  attack: 5,
+	  defence: 3,
+	  count: 1,
+	  health: 10
+	};
+
+	var banditArcher = {
+	  name: 'banditArcher',
+	  minDmg: 2,
+	  maxDmg: 3,
+	  attack: 5,
+	  defence: 2,
+	  count: 1,
+	  health: 8,
+	};
+
+	var mercSpearmen = {
+	  name: 'mercenarySpearmen',
+	  minDmg: 4,
+	  maxDmg: 6,
+	  attack: 6,
+	  defence: 7,
+	  count: 1,
+	  health: 10
+	};
+
+	var mercSwordman = {
+	  name: 'mercenarySwordman',
+	  minDmg: 5,
+	  maxDmg: 7,
+	  attack: 7,
+	  defence: 9,
+	  count: 1,
+	  health: 15,
+	};
+
+	var grolinWorker = {
+		name: 'Grolin worker',
+		minDmg: 1,
+		maxDmg: 2,
+		attack: 4,
+		defence: 2,
+		count: 1,
+		health: 6
+	};
