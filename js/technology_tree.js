0d445fc380790d12a4313c698a62a9092ad57f01
Gladyshev.Anton@selectel.ru <gladyshev.a@selectel.ru>
2022-07-25 04:01:39 +0300

    research tab is ready for enabling

--- js/technology_tree.js
+++ js/technology_tree.js
@@ -1,17 +1,21 @@
-var techs = {
+var tech_list = {
     war_propaganda: {
         id: "war_propaganda",
         type: "hire",
         img: "war_propaganda.png",
         name: "war propaganda",
-        name_loc: {
+        year: 1255,
+        season: 1,
+        prereqs: [],
+        name: {
+			"default":"war_propaganda",
         	"en-US":"war_propaganda",
-        	"ru-RU":"военная агитация"
+        	"ru-RU":"военная агитация",
         },
-        desc: "decrease price for hire and upkeep military units",
-        desc_loc: {
+        descr: {
+			"default":"decrease price for hire and upkeep military units",
         	"en-US":"decrease price for hire and upkeep military units",
-        	"ru-RU":"уменьшает цену для найма и содержания военных юнитов"
+        	"ru-RU":"уменьшает цену для найма и содержания военных юнитов",
         },
         attrs: {
         "hire cost":0.7,
@@ -21,9 +25,54 @@ var techs = {
      	"hire_cost":"relative",
      	"upkeep_cost":"relative"
      	},  
-        priceBuy: 200,
+        priceResearch: 200,
         upkeep: 5
     },
 };
-
-Object.freeze(techs);
+function research (techName) {
+	console.log("THE RESEARCH");
+			if (techName==="artillery"){
+				if (game.techArtillery===0) {
+					if (game.gold >= config.artilleryResearchCost) {
+						game.gold -= config.artilleryResearchCost;
+						game.techArtillery = 1;
+						postEventLog(locObj.techArtilleryResearched.txt, "BOLD");
+						updateUI();
+					} else {
+						//WE NEED MORE MONEY
+					}
+				} else {
+					//ALREADY LEARNED
+				}
+			}
+			//ToDo check whether we learned the tech already
+			if (game.gold >= tech_list[techName]["priceResearch"]) {
+				game.gold = game.gold-tech_list[techName]["priceResearch"];
+				let name = getTechName(techName);
+				console.log("we learned "+name); 
+			}
+		}
+function getTechName(techName){
+	if (tech_list[techName]["name"][language]===undefined) {
+		return tech_list[techName]["name"]["default"]; 
+	} else {
+		return tech_list[techName]["name"][language];
+	}
+}
+function getTechDescr(techName){
+	if (tech_list[techName]["descr"][language]===undefined) {
+		return tech_list[techName]["descr"]["default"]; 
+	} else {
+		return tech_list[techName]["descr"][language];
+	}	
+} 
+tech_tree_area = document.getElementById("available_researches");
+			for (tech in tech_list) {
+				src = 'resources/techs/'+tech_list[tech]["img"];
+				let id = tech_list[tech]["id"]
+				id = "\""+id+"\""
+				console.log(id);
+				let obj = '<img src="'+src+'"'+"onclick='research("+id+")'"+">";
+				tech_tree_area.innerHTML += obj;
+			}
+Object.freeze(tech_list);
