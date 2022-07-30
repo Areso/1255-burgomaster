77adf1a90559589300e8a0d570c4b02188905a93
Anton Gladyshev <returnstrike@yandex.com>
2022-07-30 23:52:18 +0300

    done with tech

--- js/technology_tree.js
+++ js/technology_tree.js
@@ -3,7 +3,6 @@ var tech_list = {
         id: "war_propaganda",
         type: "hire",
         img: "war_propaganda.png",
-        name: "war propaganda",
         year: 1255,
         season: 1,
         prereqs: [],
@@ -29,29 +28,20 @@ var tech_list = {
         upkeep: 5
     },
 };
-function research (techName) {
-	console.log("THE RESEARCH");
-			if (techName==="artillery"){
-				if (game.techArtillery===0) {
-					if (game.gold >= config.artilleryResearchCost) {
-						game.gold -= config.artilleryResearchCost;
-						game.techArtillery = 1;
-						postEventLog(locObj.techArtilleryResearched.txt, "BOLD");
-						updateUI();
-					} else {
-						//WE NEED MORE MONEY
-					}
-				} else {
-					//ALREADY LEARNED
-				}
-			}
-			//ToDo check whether we learned the tech already
-			if (game.gold >= tech_list[techName]["priceResearch"]) {
-				game.gold = game.gold-tech_list[techName]["priceResearch"];
-				let name = getTechName(techName);
-				console.log("we learned "+name); 
-			}
-		}
+
+function researchTech (){
+	if (game.gold >= tech_list[selectedTech]["priceResearch"]) {
+		game.gold = game.gold-tech_list[selectedTech]["priceResearch"];
+		game.techLearned.push(tech_list[selectedTech]);
+		let name = getTechName(selectedTech);
+		console.log("we learned "+name);
+		game.techLearned.push(selectedTech);
+		updateUI();
+		drawTabUniversity();
+		lblResearchHelp.innerHTML = "";
+	}
+}
+
 function getTechName(techName){
 	if (tech_list[techName]["name"][language]===undefined) {
 		return tech_list[techName]["name"]["default"]; 
@@ -65,14 +55,28 @@ function getTechDescr(techName){
 	} else {
 		return tech_list[techName]["descr"][language];
 	}	
-} 
+}
+function researchHelp(myobject) {
+	selectedTech               = myobject.id;
+	lblResearchHelp            = document.getElementById("lblReseachHelp");
+	lblResearchHelp.innerHTML  = getTechName(selectedTech)  + '<br>';
+	lblResearchHelp.innerHTML += getTechDescr(selectedTech) + '<br>';
+	lblResearchHelp.innerHTML += '<button onclick="researchTech()">Reserch</button>';
+}
+
 tech_tree_area = document.getElementById("available_researches");
-			for (tech in tech_list) {
-				src = 'resources/techs/'+tech_list[tech]["img"];
-				let id = tech_list[tech]["id"]
-				id = "\""+id+"\""
-				console.log(id);
-				let obj = '<img src="'+src+'"'+"onclick='research("+id+")'"+">";
-				tech_tree_area.innerHTML += obj;
-			}
+drawTabUniversity();
+function drawTabUniversity(){
+	tech_tree_area.innerHTML = "";
+	for (tech in tech_list) {
+		if (!game.techLearned.includes(tech_list[tech]["id"])) {
+			src = 'resources/techs/'+tech_list[tech]["img"];
+			let id = tech_list[tech]["id"]
+			id = "\""+id+"\"";
+			//let obj = '<img src="'+src+'"'+"onclick='researchHelp("+id+")'"+"onmouseover='researchHelp("+id+")'"+">";
+			let obj = '<img id='+id+' src="'+src+'"'+"onclick='researchHelp(this)'"+"onmouseover='researchHelp(this)'"+">";
+			tech_tree_area.innerHTML += obj;
+		}
+	}
+}
 Object.freeze(tech_list);
