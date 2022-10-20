b975d4fb98d0be7e442504dfa479546c941681eb
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-10-20 14:42:09 +0300

    First test with CI and page element inspections

---
+++ python/testware/locators.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+class StartPageLocators:
+
+    MENU_PANEL = (By.CLASS_NAME, "menu-panel")
+    RESOURCE_PANEL = (By.CLASS_NAME, "resource-panel")
+    CANVAS = (By.ID, "canvas")
