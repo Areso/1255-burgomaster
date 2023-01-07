108944558ab6407e3327f654f604c278f19dc5f1
Anton Gladyshev <returnstrike@yandex.com>
2023-01-07 21:02:13 +0200

    formatted autobattles

--- js/mech_autobattles.js
+++ js/mech_autobattles.js
@@ -31,51 +31,51 @@ function calcAutoBattle (attackerStack, attackerHeroAttack, defenderStack, defen
     postBattleLog(startRoundStr);
   }
   for (var attackerKey  in attackerStack.units) {
-				if (attackerStack.units.hasOwnProperty(attackerKey)) {
-					console.log('Attacker: ', attackerStack.units[attackerKey]);
-					for (var defenderKey in defenderStack.units) {
-						if (defenderStack.units.hasOwnProperty(defenderKey)) {
-							var target = defenderStack.units[defenderKey];
-							var dmg = game.calcDmg(attackerStack.units[attackerKey], attackerHeroAttack, defenderStack.units[defenderKey], defenderHeroDefence);
-							console.log('DMG before round: ', dmg);
-							dmg = Math.round(dmg);
-							console.log(dmg);
-							var attackerStr = locObj.autobattle_journal_log_dmg.txt.replace("%arg1", game.unitNameLoc(attackerStack.units[attackerKey].name)).replace("%arg2", dmg).replace("%arg3", game.unitNameLoc(target.name));
-							if (dmg >= target.stackHealth) {
-								console.log(`${target.name} is dying`);
-								delete defenderStack.units[defenderKey];
-								game.myhero[target.name] = 0;
-								console.log("%c DEFENDER UNITS LIST: ", "background: purple; color: #ededed", defenderStack.units);
-								attackerStr += ' '  + locObj.autobattle_journal_log_destroyed.txt.replace("%arg1", game.unitNameLoc(target.name));
-								if (campaignType==="AutoCampaign") {
-									postBattleLog(attackerStr);
-								}
-								if (isEmptyObj(defenderStack.units)) {
-									game.isAutoBattle = false;
-									game.checkWinner(campaignType);
-									if (campaignType === "AutoCampaign") {
-										game.attacker = 0;
-									}
-								}
-							} else {
-								var defenderLooses = parseInt(dmg / defenderStack.units[defenderKey].health);
-								defenderStack.units[defenderKey].count -= defenderLooses;
-								game.myhero[defenderKey] -= defenderLooses;
-								target.stackHealth -= dmg;
-								if (defenderLooses > 0) {
-									attackerStr += ' ' + locObj.autobattle_journal_log_dead.txt.replace("%arg1", defenderLooses).replace("%arg2", game.unitNameLoc(target.name));
-								}
-								if (campaignType==="AutoCampaign") {
-									postBattleLog(attackerStr);
-								}
-							}
-						}
-					}
-				}
-			}
-    if (campaignType==="AutoCampaign") {
-      postBattleLog(endRoundStr);
+    if (attackerStack.units.hasOwnProperty(attackerKey)) {
+      console.log('Attacker: ', attackerStack.units[attackerKey]);
+      for (var defenderKey in defenderStack.units) {
+        if (defenderStack.units.hasOwnProperty(defenderKey)) {
+          var target = defenderStack.units[defenderKey];
+          var dmg = game.calcDmg(attackerStack.units[attackerKey], attackerHeroAttack, defenderStack.units[defenderKey], defenderHeroDefence);
+          console.log('DMG before round: ', dmg);
+          dmg = Math.round(dmg);
+          console.log(dmg);
+          var attackerStr = locObj.autobattle_journal_log_dmg.txt.replace("%arg1", game.unitNameLoc(attackerStack.units[attackerKey].name)).replace("%arg2", dmg).replace("%arg3", game.unitNameLoc(target.name));
+          if (dmg >= target.stackHealth) {
+            console.log(`${target.name} is dying`);
+            delete defenderStack.units[defenderKey];
+            game.myhero[target.name] = 0;
+            console.log("%c DEFENDER UNITS LIST: ", "background: purple; color: #ededed", defenderStack.units);
+            attackerStr += ' '  + locObj.autobattle_journal_log_destroyed.txt.replace("%arg1", game.unitNameLoc(target.name));
+            if (campaignType==="AutoCampaign") {
+              postBattleLog(attackerStr);
+            }
+            if (isEmptyObj(defenderStack.units)) {
+              game.isAutoBattle = false;
+              game.checkWinner(campaignType);
+              if (campaignType === "AutoCampaign") {
+                game.attacker = 0;
+              }
+            }
+          } else {
+            var defenderLooses = parseInt(dmg / defenderStack.units[defenderKey].health);
+            defenderStack.units[defenderKey].count -= defenderLooses;
+            game.myhero[defenderKey] -= defenderLooses;
+            target.stackHealth -= dmg;
+            if (defenderLooses > 0) {
+              attackerStr += ' ' + locObj.autobattle_journal_log_dead.txt.replace("%arg1", defenderLooses).replace("%arg2", game.unitNameLoc(target.name));
+            }
+            if (campaignType==="AutoCampaign") {
+              postBattleLog(attackerStr);
+            }
+          }
+        }
+      }
     }
-    console.log('-- Attack turn ended --');
-    console.log('-----------------------');
+  }
+  if (campaignType==="AutoCampaign") {
+   postBattleLog(endRoundStr);
+  }
+  console.log('-- Attack turn ended --');
+  console.log('-----------------------');
 }
