6e8dc6de08264d22a7213aad1b3391ff9308fe93
Areso <returnstrike@yandex.ru>
2023-01-13 01:57:27 +0300

    added get_identifier script

---
+++ get_identifier.sh
@@ -0,0 +1,5 @@
+curl --request GET \
+  --url https://api.cloudflare.com/client/v4/zones \
+  --header 'Content-Type: application/json' \
+  --header 'X-Auth-Email: email' \
+  --header 'X-Auth-Key: token'
