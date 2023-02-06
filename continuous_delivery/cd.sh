3fe170581d80f6358823fc2a7d2efa792e88e43e
Anton Gladyshev <returnstrike@yandex.com>
2023-02-06 23:44:49 +0200

    updated cd script

--- continuous_delivery/cd.sh
+++ continuous_delivery/cd.sh
@@ -2,6 +2,7 @@ git_pull_result=$(git pull)
 source ~/clftoken.txt
 #after that we expected to get this variables in the env
 # identifier, clftoken, email
+# sentry_acc_id, sentry_proj_id
 expected_res="Already up-to-date."
 purgecache_command=$(cat << EOF
 curl --request POST
@@ -12,7 +13,27 @@ curl --request POST
   --data '{ "purge_everything": true }'
 EOF
 )
+
+commit_cmd(){
+ git log --stat -n 1 | head -1
+}
+commit=$(commit_cmd)
+branch_cmd(){
+ git status | head -1
+}
+branch=$(branch_cmd)
+release=$branch" "$commit
+
+sentry_release_upd=$(cat << EOF
+curl --request POST
+  --url https://sentry.io/api/hooks/release/builtin/$sentry_acc_id/$sentry_proj_id/
+  --header 'Content-Type: application/json'
+  --data '{"version": "$release"}'
+EOF
+)
+
 if [[ $git_pull_result != $expected_res ]]
 then
   eval $purgecache_command
+  eval $sentry_release_upd
 fi
