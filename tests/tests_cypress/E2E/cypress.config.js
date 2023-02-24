604395ca89fca51a49fdb0cda00202ec967e669b
aablatov <spoil90@inbox.ru>
2023-02-25 02:30:27 +0300

    tests/cypress-01. buttons in window city

--- tests/tests_cypress/E2E/cypress.config.js
+++ tests/tests_cypress/E2E/cypress.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "cypress";
 
-export default defineConfig({
+export default ({
   e2e: {
+    baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- tests/tests_cypress/E2E/cypress/e2e/buttonCity.cy.js
+++ tests/tests_cypress/E2E/cypress/e2e/buttonCity.cy.js
@@ -1,6 +1,6 @@
 describe('City', () => {
 
-    it('should be button City', () => {
+    it('should be buttons in  window City', () => {
         cy.visit('/');
 
         Cypress.on('uncaught:exception', (err, runnable) => {
@@ -13,30 +13,64 @@ describe('City', () => {
             win.eval('document.getElementById("myDCanvas").classList.remove("active-modal")');
         });
 
-        cy.get('#menu-panel').within(() => {
+        cy.get('.menu-panel').within(() => {
             cy.get('#tabCity').click();
             cy.get('#tabCity').should('have.class','tab-link active');
             cy.get('#btnOpenTabBuilding').should('have.class','tab-link');
             cy.get('#tabSettings').should('have.class','tab-link');
             cy.get('#tabAbout').should('have.class','tab-link');
             cy.get('#tabDiscord').should('have.class','tab-link');
-            cy.get('#lblEventCountdownValue').should('have.class','tab-link');
-            cy.get('#_blank').should('have.href','https://www.patreon.com/bePatron?u=7838459');
+            cy.get('#lblEventCountdownValue');
         });
-        cy.get('#resource-panel').within(() => {
 
+        cy.get('.resource-panel').within(() => {
+            cy.get('.gold-val').within(() => {
+                cy.get('#gold_img_bt').should('have.class','resource-icon')
+              //  cy.get('#gold').should('have.style','display:none')
+                cy.get('#panelGoldValue').contains(/\d+/);
+            });
+            cy.get('.population-val').within(() => {
+                cy.get('#pop_img').should('have.class','resource-icon')
+                cy.get('#panelPopValue').contains(/\d+/);
+            });
+            cy.get('.gems-val').within(() => {
+                cy.get('#gems_img').should('have.class','resource-icon')
+                cy.get('#gems').contains(/\d+/);
+            });
+            cy.get('.time-val').within(() => {
+                cy.get('.resource-icon');
+                cy.get('#dcounter').contains(/\d+/);
+            });
+            cy.get('.server-val').within(() => {
+                cy.get('#spnServerStatusLabel');
+                cy.get('#spnServerStatusValue');
+            });
+            cy.get('.event-val').within(() => {
+                cy.get('#event-label');
+                cy.get('#event-value');
+            });
         });
-        cy.get('#content-panel').within(() => {
+
+        cy.get('.content-panel').within(() => {
             cy.get('#main').within(() => {
-
+                cy.get('#canvas');
+                cy.get('#saveGameButton').should('have.class','btn');
+                cy.get('#saveGameButton').click();
+                cy.get('#loadGameButton').should('have.class','btn');
             });
         });
-        cy.get('#info-panel').within(() => {
-            cy.get('#info-panel__actions').within(() => {
 
+        cy.get('.info-panel').within(() => {
+            cy.get('.info-panel__actions').within(() => {
+                cy.get('#log_btn').should('have.class','btn');
+                cy.get('#chat_btn').should('have.class','btn');
+                cy.get('#chat_btn').should('have.class','btn');
+                cy.get('#lbl_online').within(() => {
+                    cy.get('#spnOnline');
+                    cy.get('#spnOnlineValue');                  
+                });    
             });
-
+            cy.get('#log').should('have.class','info-panel__history');
         });
-
     });
 })
