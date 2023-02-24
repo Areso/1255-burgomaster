fded0c0e57b26a98671f0eee5f15d7eee5a46051
aablatov <spoil90@inbox.ru>
2023-02-24 23:38:59 +0300

    tests/cypress-00.Update cypress.config.js

--- tests/tests_cypress/validateHTML/cypress.config.js
+++ tests/tests_cypress/validateHTML/cypress.config.js
@@ -2,15 +2,6 @@ import htmlvalidate from "cypress-html-validate/plugin";
 
 export default {
   e2e: {
-    reporter: 'mochawesome',
-    reporterOptions: {
-      html: false,
-      json: true,
-      reportDir: "cypress/reports/",
-      reportFilename: "[status]_[datetime]-[name]-report",
-      timestamp: "longDate",
-      toConsole: true
-    },
     baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {         
         htmlvalidate.install(on);
