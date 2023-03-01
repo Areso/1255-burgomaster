f8417c6d14df4c9da2c70b0b5e5705f6d0bed800
aablatov <spoil90@inbox.ru>
2023-03-01 09:52:43 +0300

    tests/cypress-02-1. disabling video

--- tests/tests_cypress/validateHTML/cypress.config.js
+++ tests/tests_cypress/validateHTML/cypress.config.js
@@ -3,7 +3,8 @@ import htmlvalidate from "cypress-html-validate/plugin";
 export default {
   e2e: {
     baseUrl: 'http://localhost:8080',
-    setupNodeEvents(on, config) {         
+    setupNodeEvents(on, config) {
+        video: false;         
         htmlvalidate.install(on);
       // implement node event listeners here
     },
